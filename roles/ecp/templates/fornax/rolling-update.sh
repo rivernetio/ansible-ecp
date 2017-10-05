38b5c41d43d753988974bf2bce81eede49b94127
Meng Ding <dingmeng@gmail.com>
2017-10-05 22:38:20 +0000

    Initial commit

---
+++ roles/ecp/templates/fornax/rolling-update.sh
@@ -0,0 +1,6 @@
+# !/bin/sh
+pod_name=`kubectl get pods --namespace=sky-firmament |grep firmament-fornax|awk '{ print $1 }'`
+
+echo $pod_name
+
+kubectl delete pods $pod_name --namespace=sky-firmament
